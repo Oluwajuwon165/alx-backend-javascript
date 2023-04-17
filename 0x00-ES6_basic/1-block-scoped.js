a231531ea7ca94033fe89a2155497221430696b2
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-04-17 19:01:43 +0100

    Using let and const to modify some variables in a function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,13 @@
+/*Modifying the variables inside the function taskBlock so that the 
+variables aren’t overwritten inside the conditional block.*/
+
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
