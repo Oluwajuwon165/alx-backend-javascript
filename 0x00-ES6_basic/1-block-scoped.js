934347e56d51f3992d0ab89710fbd30ae04f66c8
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-04-17 19:13:15 +0100

    Modifying some funcions variable

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
