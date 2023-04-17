63fa8d4894b26b4479a9d7857926de4ace7ae583
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-04-17 17:46:30 +0100

    Using let to modify some variables in a function

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task_jhay = true;
-    let task2_jhay = false;	  
-    task = task_jhay;	  
-    task2 = task2_jhay;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
