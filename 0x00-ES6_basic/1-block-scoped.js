cd0c1df778b9c95d0b67ce18b778869404b24edc
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-04-18 13:38:30 +0100

    fixing eslint errors

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
-//Modifying the variables inside the function taskBlock so that the 
-//variables aren’t overwritten inside the conditional block.
+// Modifying the variables inside the function taskBlock so that the
+// variables aren’t overwritten inside the conditional block.
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
