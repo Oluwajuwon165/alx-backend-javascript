9797419cdabd2499fa1649badc0b3a8bd76aa900
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-04-22 11:06:09 +0100

    Handling multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,15 +3,31 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   try {
-    const user = await signUpUser(firstName, lastName);
-    const photo = await uploadPhoto(fileName);
-    return [
-      { status: 'fulfilled', value: user },
-      { status: 'fulfilled', value: photo },
+    const [signUpUserResponse, uploadPhotoResponse] = await Promise.allSettled([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
+    const result = [
+      {
+        status: signUpUserResponse.status,
+        value:
+          signUpUserResponse.status === 'rejected'
+            ? `${signUpUserResponse.reason}`
+            : signUpUserResponse.value,
+      },
+      {
+        status: uploadPhotoResponse.status,
+        value:
+          uploadPhotoResponse.status === 'rejected'
+            ? `${uploadPhotoResponse.reason}`
+            : uploadPhotoResponse.value,
+      },
     ];
+    return result;
   } catch (error) {
-    return [
-      { status: 'rejected', reason: error },
-    ];
+    console.error(error);
   }
+
+  // Add a default return statement here
+  return null;
 }
