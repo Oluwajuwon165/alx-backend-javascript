99afb4c2935cd6ebdc1453609070b125ba950074
Oluwajuwon165 <Oladitioluwajuwon165@gmail.com>
2023-04-24 17:17:51 +0100

    Implementing a class named building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,5 +1,8 @@
 class Building {
   constructor(sqft) {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
